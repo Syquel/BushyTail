8a51a42fdca5107a61c5208b46c18a4e09acacf1
Clemens Bartz <clemens@clemens-bartz.de>
2016-03-05 01:54:08 +0100

    Add BushyTailEntityProcessor.

---
+++ src/main/java/de/syquel/bushytail/service/BushyTailEntityProcessor.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016, Frederik Boster
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.syquel.bushytail.service;
+
+import org.apache.olingo.commons.api.format.ContentType;
+import org.apache.olingo.server.api.OData;
+import org.apache.olingo.server.api.ODataApplicationException;
+import org.apache.olingo.server.api.ODataLibraryException;
+import org.apache.olingo.server.api.ODataRequest;
+import org.apache.olingo.server.api.ODataResponse;
+import org.apache.olingo.server.api.ServiceMetadata;
+import org.apache.olingo.server.api.processor.EntityProcessor;
+import org.apache.olingo.server.api.uri.UriInfo;
+
+/**
+ * Processes CRUD operations on a single Olingo entity.
+ *
+ * @author Clemens Bartz
+ * @since 1.0
+ */
+public class BushyTailEntityProcessor implements EntityProcessor {
+    public void readEntity(ODataRequest oDataRequest, ODataResponse oDataResponse, UriInfo uriInfo, ContentType contentType) throws ODataApplicationException, ODataLibraryException {
+        
+    }
+
+    public void createEntity(ODataRequest oDataRequest, ODataResponse oDataResponse, UriInfo uriInfo, ContentType contentType, ContentType contentType1) throws ODataApplicationException, ODataLibraryException {
+
+    }
+
+    public void updateEntity(ODataRequest oDataRequest, ODataResponse oDataResponse, UriInfo uriInfo, ContentType contentType, ContentType contentType1) throws ODataApplicationException, ODataLibraryException {
+
+    }
+
+    public void deleteEntity(ODataRequest oDataRequest, ODataResponse oDataResponse, UriInfo uriInfo) throws ODataApplicationException, ODataLibraryException {
+
+    }
+
+    public void init(OData oData, ServiceMetadata serviceMetadata) {
+
+    }
+}
