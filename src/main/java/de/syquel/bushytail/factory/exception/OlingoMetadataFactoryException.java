8e5de161f4e5fa54386427ca9c9e19cc3c81a88d
Clemens Bartz <clemens@clemens-bartz.de>
2016-09-22 22:39:20 +0200

    Add exception for OlingoMetadataFactory.

---
+++ src/main/java/de/syquel/bushytail/factory/exception/OlingoMetadataFactoryException.java
@@ -0,0 +1,11 @@
+package de.syquel.bushytail.factory.exception;
+
+import de.syquel.bushytail.exception.BushyTailException;
+
+/**
+ * Exception for the {@link de.syquel.bushytail.factory.OlingoMetadataFactory}.
+ *
+ * @author Clemens Bartz
+ */
+public class OlingoMetadataFactoryException extends BushyTailException {
+}
