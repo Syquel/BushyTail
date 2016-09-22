a2a968b5dbcc3ddd453d2bde333548f9c3967167
Clemens Bartz <clemens@clemens-bartz.de>
2016-09-22 22:36:36 +0200

    Extend exceptions from BushyTailException.

--- src/main/java/de/syquel/bushytail/serializer/exception/OlingoSerializerException.java
+++ src/main/java/de/syquel/bushytail/serializer/exception/OlingoSerializerException.java
@@ -16,63 +16,8 @@
 
 package de.syquel.bushytail.serializer.exception;
 
-import java.security.PrivilegedActionException;
+import de.syquel.bushytail.exception.BushyTailException;
 
-public class OlingoSerializerException extends Exception {
+public class OlingoSerializerException extends BushyTailException {
 
-    /**
-     * Constructs a new exception with <code>null</code> as its detail message.
-     * The cause is not initialized, and may subsequently be initialized by a
-     * call to {@link #initCause}.
-     */
-    public OlingoSerializerException() {
-    }
-
-    /**
-     * Constructs a new exception with the specified detail message.  The
-     * cause is not initialized, and may subsequently be initialized by
-     * a call to {@link #initCause}.
-     *
-     * @param message the detail message. The detail message is saved for
-     *                later retrieval by the {@link #getMessage()} method.
-     */
-    public OlingoSerializerException(String message) {
-        super(message);
-    }
-
-    /**
-     * Constructs a new exception with the specified detail message and
-     * cause.  <p>Note that the detail message associated with
-     * <code>cause</code> is <i>not</i> automatically incorporated in
-     * this exception's detail message.
-     *
-     * @param message the detail message (which is saved for later retrieval
-     *                by the {@link #getMessage()} method).
-     * @param cause   the cause (which is saved for later retrieval by the
-     *                {@link #getCause()} method).  (A <tt>null</tt> value is
-     *                permitted, and indicates that the cause is nonexistent or
-     *                unknown.)
-     * @since 1.4
-     */
-    public OlingoSerializerException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    /**
-     * Constructs a new exception with the specified cause and a detail
-     * message of <tt>(cause==null ? null : cause.toString())</tt> (which
-     * typically contains the class and detail message of <tt>cause</tt>).
-     * This constructor is useful for exceptions that are little more than
-     * wrappers for other throwables (for example, {@link
-     * PrivilegedActionException}).
-     *
-     * @param cause the cause (which is saved for later retrieval by the
-     *              {@link #getCause()} method).  (A <tt>null</tt> value is
-     *              permitted, and indicates that the cause is nonexistent or
-     *              unknown.)
-     * @since 1.4
-     */
-    public OlingoSerializerException(Throwable cause) {
-        super(cause);
-    }
 }
